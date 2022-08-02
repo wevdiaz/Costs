76833fd881cfc2a1b2132b0b6093389b28651d30
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-02 17:20:57 -0300

    criação da Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,16 @@
+import styles from "./styles/Home.module.css";
+import savings from "../../imgs/savings.svg";
+import { LinkButton } from "../layout/LinkButton";
 
 export function Home() {
   return (
-    <h1>Home</h1>
+   <section className={styles.home_container}>
+    <h1>
+      Bem-vindo ao <span>Costs</span>
+    </h1>
+    <p>Comece a gerenciar os seus próprios projetos agora mesmo.</p>
+    <LinkButton to="/newproject" text="Criar Projeto" />
+    <img src={savings} alt="Costs" />
+   </section>
   );
 }
