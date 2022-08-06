d626ed2c340d16e7b5a9e35533140a0e751e70bb
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-06 17:15:18 -0300

    resgatando dados para a página projetos

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,18 +1,39 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import { Message } from "../layout/Message";
 import { Container } from "../layout/Container";
 import { LinkButton } from "../layout/LinkButton";
+import { ProjectCard } from "../project/ProjectCard";
 
 import styles from "./styles/Projects.module.css";
 
 export function Projects() {
+
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
 
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setProjects(data);
+      console.log();
+    }).catch((err) => console.log(err));
+
+  }, []);
   
   return (
     <div className={styles.project_container}>
@@ -24,7 +45,21 @@ export function Projects() {
         message && <Message msg={message} type={"success"} />
       }
       <Container customClass="start">
-        <p>Projetos</p>
+        {
+          projects.length > 0 ? (
+            projects.map((project) => (
+              <ProjectCard 
+                  name={project.name}
+                  id={project.id}
+                  budget={project.budget} 
+                  category={project.category.name}
+                  key={project.id}
+              />
+            ))
+          ) : (
+            <p>Não nenhum projeto no momento.</p>
+          )
+        }
       </Container>
     </div>
   );
