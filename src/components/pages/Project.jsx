6718ee60144e5dfe46ab4887755d953e63d5535c
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-15 16:48:05 -0300

    update db

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { parse, v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 import { Loading } from "../layout/Loading";
 import { Container } from "../layout/Container";
